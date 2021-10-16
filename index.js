450063e38b9868597fbcfbf2ff8d91c0d2c1ddf6
boogiemedia <speedysokol@gmail.com>
2021-10-17 01:50:04 +0300

    starter

---
+++ index.js
@@ -0,0 +1,7 @@
+const profileName = document.querySelector('.profile__info')
+const profileSubInfo = document.querySelector('.profile__sub-info')
+const profileEdditor = document.querySelector('.profile__edit-button')
+
+profileEdditor.addEventListener('click', () =>{
+    console.log("test");
+})
