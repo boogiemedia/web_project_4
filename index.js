fedf0785bcc24d0bf3107d0a861bdb040ed8824e
boogiemedia <speedysokol@gmail.com>
2021-10-17 22:39:01 +0300

    99

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-const profileName = document.querySelector('.profile__info')
-const profileSubInfo = document.querySelector('.profile__sub-info')
-const profileEdditor = document.querySelector('.profile__edit-button')
 
-profileEdditor.addEventListener('click', () =>{
-    console.log("test");
+// text 
+const profileName = document.querySelector(".profile__info");
+const profileSubInfo = document.querySelector(".profile__sub-info");
+//buttons
+const profileEdditor = document.querySelector(".profile__edit-button");
+const popup = document.querySelector(".popup");
+const closeButton = document.querySelector(".popup__close-button");
+const submitButton = popup.querySelector('.popup__save-button')
+//text changable fields
+const nameChanger = document.querySelector('.popup__input_type_name');
+const descriptionChanger = document.querySelector('.popup__input_type_description');
+
+profileEdditor.addEventListener("click", () => {
+    nameChanger.value = profileName.textContent;
+    descriptionChanger.value = profileSubInfo.textContent;
+  openPopup();
+});
+submitButton.addEventListener("click", () => {
+    profileName.textContent = nameChanger.value;
+    profileSubInfo.textContent = descriptionChanger.value;
+    closePopup();
 })
+function openPopup() {
+  popup.classList.add("popup_oppened");
+}
+
+closeButton.addEventListener("click", () => {
+    closePopup()
+})
+
+function closePopup() {
+    popup.classList.remove("popup_oppened");
+}
