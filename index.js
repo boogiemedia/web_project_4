8fb073f147a7ac7e0aaae983e84ec98a716c10a6
boogiemedia <speedysokol@gmail.com>
2021-10-21 14:48:15 +0300

    check for error

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-
-// text 
+// text
 const profileName = document.querySelector(".profile__info");
 const profileSubInfo = document.querySelector(".profile__sub-info");
 //buttons
 const profileEdditor = document.querySelector(".profile__edit-button");
 const popup = document.querySelector(".popup");
 const closeButton = document.querySelector(".popup__close-button");
-const submitButton = popup.querySelector('.popup__save-button')
+const submitButton = popup.querySelector(".popup__save-button");
 //text changable fields
-const nameChanger = document.querySelector('.popup__input_type_name');
-const descriptionChanger = document.querySelector('.popup__input_type_description');
+const nameChanger = document.querySelector(".popup__input_type_name");
+const descriptionChanger = document.querySelector(
+  ".popup__input_type_description"
+);
 
 profileEdditor.addEventListener("click", () => {
-    nameChanger.value = profileName.textContent;
-    descriptionChanger.value = profileSubInfo.textContent;
   openPopup();
+  nameChanger.value = profileName.textContent;
+  descriptionChanger.value = profileSubInfo.textContent;
 });
 submitButton.addEventListener("click", () => {
-    profileName.textContent = nameChanger.value;
-    profileSubInfo.textContent = descriptionChanger.value;
-    closePopup();
-})
+  closePopup();
+  profileName.textContent = nameChanger.value;
+  profileSubInfo.textContent = descriptionChanger.value;
+});
 function openPopup() {
   popup.classList.add("popup_oppened");
 }
 
 closeButton.addEventListener("click", () => {
-    closePopup()
-})
+  closePopup();
+});
 
 function closePopup() {
-    popup.classList.remove("popup_oppened");
+  popup.classList.remove("popup_oppened");
 }
