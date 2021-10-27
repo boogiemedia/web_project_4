5accfa09c49188685fa4349e45c81b9da08a5255
boogiemedia <speedysokol@gmail.com>
2021-10-27 22:41:39 +0300

    start 5th project yay

--- script/index.js
+++ script/index.js
@@ -3,16 +3,22 @@ const profileName = document.querySelector(".profile__info");
 const profileSubInfo = document.querySelector(".profile__sub-info");
 //buttons
 const profileEdditor = document.querySelector(".profile__edit-button");
-const popUp = document.querySelector(".popup");
-const closeButton = document.querySelector(".popup__close-button");
+const cardEdditor = document.querySelector(".profile__add-button");
+const popUp = document.querySelectorAll(".popup");
+const popUpPerson = document.querySelector(".popup_person");
+const popupAdd = document.querySelector(".popup_card");
+const closeButton = document.querySelectorAll(".popup__close-button");
 //text changable fields
 const nameChanger = document.querySelector(".popup__input_type_name");
 const descriptionChanger = document.querySelector(".popup__input_type_description");
 //end of variables*************************************************************************
 
 
-function openPopup() {
-  popUp.classList.add("popup_oppened");
+function openPopupPerson() {
+  popUpPerson.classList.add("popup_oppened");
+}
+function openPopupAdd() {
+  popupAdd.classList.add("popup_oppened");
 }
 
 function closePopup() {
@@ -22,10 +28,15 @@ function closePopup() {
 
 //open profile edditor
 profileEdditor.addEventListener("click", () => {
-  openPopup();
+  openPopupPerson();
   nameChanger.value = profileName.textContent;
   descriptionChanger.value = profileSubInfo.textContent;
 });
+//open Card edditor
+cardEdditor.addEventListener("click", () => {
+  openPopupAdd();
+});
+
 //submit the changes and close pupup
 popUp.addEventListener("submit", (Event) => {
   Event.preventDefault();
