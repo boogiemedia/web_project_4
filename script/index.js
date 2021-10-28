ae0946c3aebbf6a2fcb852847928c03dd9ef003c
boogiemedia <speedysokol@gmail.com>
2021-10-28 13:57:57 +0300

    popups eddited

--- script/index.js
+++ script/index.js
@@ -2,40 +2,59 @@
 const profileName = document.querySelector(".profile__info");
 const profileSubInfo = document.querySelector(".profile__sub-info");
 //buttons
+const popup =document.querySelector("popup");
 const profileEdditor = document.querySelector(".profile__edit-button");
-const popUp = document.querySelector(".popup");
-const closeButton = document.querySelector(".popup__close-button");
+const ProfileAddCard = document.querySelector(".profile__add-button")
+const closeButton = document.querySelectorAll(".popup__close-button");
+
+//popups
+const popUp =document.querySelectorAll(".popup");
+const popUpProfileEdditor = document.querySelector(".popup_type_profile-edditor");
+const popUpCardEdditor = document.querySelector(".popup_type_card-edditor");
 //text changable fields
 const nameChanger = document.querySelector(".popup__input_type_name");
 const descriptionChanger = document.querySelector(".popup__input_type_description");
 //end of variables*************************************************************************
 
-
-function openPopup() {
-  popUp.classList.add("popup_oppened");
+//profile edditor oppener
+function openProfileEdditor() {
+  popUpProfileEdditor.classList.add("popup_oppened");
+}
+//card edditor oppener
+function openCardEdditor() {
+  popUpCardEdditor.classList.add("popup_oppened");
 }
 
-function closePopup() {
-  popUp.classList.remove("popup_oppened");
-}
 //end of functions**************************************************************************
 
-//open profile edditor
+// profile edditor btn
 profileEdditor.addEventListener("click", () => {
-  openPopup();
+  openProfileEdditor();
   nameChanger.value = profileName.textContent;
   descriptionChanger.value = profileSubInfo.textContent;
 });
+
+//card edditor btn
+ProfileAddCard.addEventListener("click", () => {
+  openCardEdditor();
+  
+});
+
 //submit the changes and close pupup
-popUp.addEventListener("submit", (event) => {
+//submit profile
+popUpProfileEdditor.addEventListener("submit", (event) => {
   event.preventDefault();
-  closePopup();
   profileName.textContent = nameChanger.value;
   profileSubInfo.textContent = descriptionChanger.value;
 });
-//close button
-closeButton.addEventListener("click", () => {
-  closePopup();
+//submit profile
+popUpCardEdditor.addEventListener("submit", (event) => {
+  event.preventDefault();
 });
+//close button
+closeButton.forEach(btn => btn.addEventListener("click",() => {
+  popUp.forEach(pop => pop.classList.remove("popup_oppened"))
+  
+}));
 
 //end of EventListener***********************************************************************
