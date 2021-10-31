fb8a2ffaa3d9de44308d4a66fbb945c9325ce4f3
boogiemedia <speedysokol@gmail.com>
2021-10-31 13:43:22 +0200

    lets do some review )

--- script/index.js
+++ script/index.js
@@ -3,113 +3,127 @@ const profileName = document.querySelector(".profile__info");
 const profileSubInfo = document.querySelector(".profile__sub-info");
 //text changable fields
 const nameChanger = document.querySelector(".popup__input_type_name");
-const descriptionChanger = document.querySelector(".popup__input_type_description");
+const descriptionChanger = document.querySelector(
+  ".popup__input_type_description"
+);
 const cardName = document.querySelector(".popup__input_type_title");
 const cardLink = document.querySelector(".popup__input_type_link");
 //buttons
 const profileEdditor = document.querySelector(".profile__edit-button");
-const ProfileAddCard = document.querySelector(".profile__add-button")
+const ProfileAddCard = document.querySelector(".profile__add-button");
 const closeButton = document.querySelectorAll(".popup__close-button");
 const SubmitBtn = document.querySelectorAll(".popup__save-button");
 const trash = document.querySelectorAll(".elements__trash");
 //popups
-const popUp =document.querySelectorAll(".popup");
+const popUp = document.querySelectorAll(".popup");
 const popUpProfileEdditor = document.querySelector(".popup_type_profile-edditor");
 const popUpCardEdditor = document.querySelector(".popup_type_card-edditor");
+const popUpPreview = document.querySelector(".popup_type_preview");
+
 //cards elements
 const elementsPlace = document.querySelector(".elements");
-const elementTemplate = document.querySelector(".elements__template").content.querySelector(".elements__block");
+const elementTemplate = document
+  .querySelector(".elements__template")
+  .content.querySelector(".elements__block");
 //end of variables*************************************************************************
 
 //Cards
 const initialElements = [
   {
     name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
   },
   {
     name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
   },
   {
     name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
   },
   {
     name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
   },
   {
     name: "Vanoise National Park",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
   },
   {
     name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg"
-  }
-]; 
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
 
 // end of card list****************************************************************************
 
 //profile edditor oppener
-function openProfileEdditor() {
-  popUpProfileEdditor.classList.add("popup_oppened");
-}
-//card edditor oppener
-function openCardEdditor() {
-  popUpCardEdditor.classList.add("popup_oppened");
-}
 
+function openPopUp(popUp) {
+  popUp.classList.add("popup_oppened");
+}
 
 // card edditor
-function addCardElement (elementData) {
+function addCardElement(elementData) {
   const element = elementTemplate.cloneNode(true);
   element.querySelector(".elements__title").textContent = elementData.name;
-  element.querySelector(".elements__cover").style.backgroundImage = `url(${elementData.link})`;
-  
-  element.querySelector(".elements__trash").addEventListener('click', () => {
+  element.querySelector(
+    ".elements__cover"
+  ).style.backgroundImage = `url(${elementData.link})`;
+
+  element.querySelector(".elements__trash").addEventListener("click", () => {
     element.remove();
   });
-  element.querySelector(".elements__like").addEventListener('click', (event) => {
-    event.target.classList.add("elements__like_type_active");
-});
+  //like btn
+  element
+    .querySelector(".elements__like")
+    .addEventListener("click", (event) => {
+      event.target.classList.add("elements__like_type_active");
+    });
+  //opne preview
+  element.querySelector(".elements__cover").addEventListener("click", () => {
+    openPopUp(popUpPreview);
+    popUpPreview.querySelector(".popup__preview-text").textContent = elementData.name;
+    popUpPreview.querySelector(".popup__preview").alt = elementData.name;
+    popUpPreview.querySelector(".popup__preview").src = elementData.link;
+  });
+
   return element;
 }
 
-initialElements.forEach(initialElements =>{
+
+initialElements.forEach((initialElements) => {
   elementsPlace.prepend(addCardElement(initialElements));
-})
+});
 
 function newCardElement() {
- const addNewCard = addCardElement({
-        name: cardName.value,
-          link: cardLink.value
-    });
-    elementsPlace.prepend(addNewCard);
-
-  }
+  const addNewCard = addCardElement({
+    name: cardName.value,
+    link: cardLink.value,
+  });
+  elementsPlace.prepend(addNewCard);
+}
 //end of functions**************************************************************************
 
 // profile edditor btn
 profileEdditor.addEventListener("click", () => {
-  openProfileEdditor();  
+  openPopUp(popUpProfileEdditor);
   nameChanger.value = profileName.textContent;
   descriptionChanger.value = profileSubInfo.textContent;
 });
 
-
 //card edditor btn
 ProfileAddCard.addEventListener("click", () => {
-  openCardEdditor();
+  openPopUp(popUpCardEdditor);
 });
-//end of btns**************************************************************************
 
+//end of btns**************************************************************************
 
 //submit Card
 popUpCardEdditor.addEventListener("submit", (event) => {
   event.preventDefault();
   newCardElement();
-  });
+});
 
 //submit profile
 popUpProfileEdditor.addEventListener("submit", (event) => {
@@ -119,11 +133,15 @@ popUpProfileEdditor.addEventListener("submit", (event) => {
 });
 
 //close elements
-  SubmitBtn.forEach(btn => btn.addEventListener("click",() => {
-    popUp.forEach(pop => pop.classList.remove("popup_oppened"))
-  }));
-closeButton.forEach(btn => btn.addEventListener("click",() => {
-  popUp.forEach(pop => pop.classList.remove("popup_oppened"))
-}));
+SubmitBtn.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    popUp.forEach((pop) => pop.classList.remove("popup_oppened"));
+  })
+);
+closeButton.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    popUp.forEach((pop) => pop.classList.remove("popup_oppened"));
+  })
+);
 
 //end of EventListener***********************************************************************
